8112923cca8ce5b58bddb21d8c8da84adfcb14ae
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-09 16:53:02 +0800

    set the input file path

---
+++ src/model/LoadModel.java
@@ -0,0 +1,15 @@
+package model;
+
+public class LoadModel {
+	public static final String wordListPath = "data/1-2orderhanzi.txt"; //一二级汉字表
+	public static final String wordPinyinListPath = "data/pinyin-hanzi.txt"; //拼音汉字表
+	public static final String[] datasetName = {"2016-01.txt", "2016-02.txt", "2016-03.txt", "2016-04.txt", "2016-05.txt",
+			"2016-06.txt", "2016-07.txt", "2016-08.txt", "2016-09.txt", "2016-10.txt", "2016-11.txt"}; //训练数据集的名字
+	public static final String datasetPath = "input/sina_news/"; //训练数据集的路径
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
